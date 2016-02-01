9c3131663cf53ada17ea5b6baf8013e52a7ea9fa
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-01 12:05:35 -0500

    Changes "motors" object to "engines" (for semantic compatibility)

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ module.exports = loader = function SuperLoader(content) {
       next = this.async();
 
   // The motors engine will compile based on file extension (we remove the leading ".")
-  loader.motors.compile(ext.replace(/^\./, ""), content, {}, next);
+  loader.engines.compile(ext, content, {}, next);
 
 }
 
 
-// Expose motors object. This allows additional/removal of motors, as
-// well as querying current configuration.
-loader.motors = new Motors();
+// Expose motors object. This allows additional/removal of motors, as well as querying
+// current configuration.
+loader.engines = new Motors();
